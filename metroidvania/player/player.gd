df310937c5edaf207323b7267727d6bdf593b9f4
pjz987 <pwj2012@gmail.com>
2023-08-20 19:51:54 -0700

    :camera:

--- metroidvania/player/player.gd
+++ metroidvania/player/player.gd
@@ -1,28 +1,55 @@
 extends CharacterBody2D
 
+@export var acceleration = 512
+@export var max_velocity = 64
+@export var friction = 256
+@export var gravity = 200
+@export var jump_force = 128
+@export var max_fall_velocity = 128
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -200.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@onready var animation_player = $AnimationPlayer
+@onready var sprite_2d = $Sprite2D
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	apply_gravity(delta)
+	var input_axis = Input.get_axis('ui_left', 'ui_right')
+	if is_moving(input_axis):
+		apply_horizontal_force(delta, input_axis)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		apply_friction(delta)
+	jump_check()
+	update_animations(input_axis)
 	move_and_slide()
+
+func is_moving(input_axis):
+	return input_axis != 0
+
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y = move_toward(velocity.y, max_fall_velocity, gravity * delta)
+
+func apply_horizontal_force(delta, input_axis):
+	velocity.x = move_toward(
+		velocity.x, input_axis * max_velocity, acceleration * delta)
+
+func apply_friction(delta):
+	velocity.x = move_toward(velocity.x, 0, friction * delta)
+
+func jump_check():
+	if is_on_floor():
+		if Input.is_action_just_pressed('ui_up') and is_on_floor():
+			velocity.y = -jump_force
+	else:
+		if (Input.is_action_just_released('ui_up')
+			and velocity.y < -jump_force / 2):
+			velocity.y = -jump_force / 2
+
+func update_animations(input_axis):
+	if is_moving(input_axis):
+		animation_player.play('run')
+		sprite_2d.scale.x = sign(input_axis)
+	else:
+		animation_player.play('idle')
+	
+	if not is_on_floor():
+		animation_player.play('jump')
